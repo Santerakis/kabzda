41017b726284e013d3edc4388dc213b882b9998b
Sergey <santerakis@gmail.com>
2024-02-21 00:02:11 +0200

    select

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -2,6 +2,7 @@ import type {Meta} from '@storybook/react';
 
 import {Select} from "./Select";
 import {action} from "@storybook/addon-actions";
+import {useState} from "react";
 
 const meta: Meta<typeof Select> = {
     title: 'Select',
@@ -10,7 +11,15 @@ const meta: Meta<typeof Select> = {
 export default meta;
 
 export const BaseExample = () => {
-    return <Select onChange={action('Value changed')} value={'1'} items={[
+    const [value, setValue] = useState('2')
+    return <Select onChange={setValue} value={value} items={[
+        {value: '1', title: 'Minsk'},
+        {value: '2', title: 'Moscow'},
+        {value: '3', title: 'Kiev'},
+    ]} />
+}
+export const WithoutValue = () => {
+    return <Select onChange={action('Value changed')} items={[
         {value: '1', title: 'Minsk'},
         {value: '2', title: 'Moscow'},
         {value: '3', title: 'Kiev'},
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import {Item} from "../Accordion/Accordion";
+import React, {useState} from 'react';
+import s from './Select.module.css'
+import {action} from "@storybook/addon-actions";
+import arrow from './arrow-down.svg'
 
 type SelectItem = {
     value: string
@@ -7,16 +9,31 @@ type SelectItem = {
 }
 
 type Select = {
-    value: any
+    value?: any
     onChange: (_: any) => void
     items: SelectItem[]
 }
 
 export const Select = (props: Select) => {
+    const [value, setValue] = useState('1')
+    const selectedItemTitle = props.items.find(i => i.value === (props.value || value))?.title
+    const [open, setOpen] = useState(false)
+
     return (
-        <div>
-            <div></div>
-            {props.items.map(i => <div>{i.title}</div>)}
+        <div className={s.select}>
+            <div onClick={() => setOpen(!open)} className={s.titltewrap}>
+                <h3 className={s.title}>{selectedItemTitle}</h3>
+                <img src={arrow} alt="logo" width="10" height="10"/>
+            </div>
+            <div className={s.items + ' ' + (open ? s.open : '')}>
+                {props.items.map((i, index) => <div key={index} style={{cursor: 'pointer'}} onClick={() => {
+                    props.onChange(i.value)
+                    setOpen(false)
+                    action('value changed on: ')(i.value)
+                    if (!props.value) setValue(i.value)
+                }}>{i.title}</div>)}
+            </div>
+
         </div>
     );
 };
